b20cfe4e83749ea306d352ac0c3854c9d1add064
Justin David Hofmann <justindhofmann@gmail.com>
2019-10-09 21:50:29 -0400

    js comments

--- js/breaksFromNews.js
+++ js/breaksFromNews.js
@@ -8,12 +8,13 @@ let STORE = [
     'get': true
   }
 ];
+
 const catUrl = "https://api.thecatapi.com/v1/images/search?";
 const ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/";
 let alternateRequestCounter = 0;
 
+
 function checkCounter() {
-  //console.log(alternateRequestCounter);
   if ( alternateRequestCounter < STORE.length) {
     return alternateRequestCounter;
   }
@@ -22,6 +23,9 @@ function checkCounter() {
     return alternateRequestCounter;
   }
 }
+
+// GET ALTERNATES TO NEWS STORIES
+
 async function alternateRequest() {
   checkCounter();
   if ( STORE[alternateRequestCounter].get === true) { await eval(
@@ -43,6 +47,7 @@ async function catRequest() {
     $('.error-message').text(`Something in catRequest went wrong: ${err.message}`);
   }
 }
+
 async function requestRonQuote() {
   try {
     const response = await fetch(ronUrl);
@@ -52,16 +57,16 @@ async function requestRonQuote() {
     $('.error-message').text(`Something went wrong: ${err.message}`);
   }
 }
+
 function displayCatResults(catJson) {
   $('.results-list').append(
     `<li><div class='alternate-container'><img class="catImg" src="${catJson[0].url}"></div><li>`
   );
-  //console.log('`displayCatResults` has run');
 }
+
 function displayRonResults(json) {
   $('.results-list').append(
     `<li><div class='alternate-container'><div class="Swanson Container">
     </div><h4 aria-label="Quote from Ron Swanson" class="ron-quote">${json}</h4></div><li>`
   );
-  //console.log('`displayRonResults` has run');
 }
